6d7a1d546ccf5cbe40ae2148b020a4c26cfa08bc
David Lefever <dl@taktik.be>
2015-08-21 10:50:32 +0200

    [IMP] handle psycopg DatabaseError in sql_db.py.

--- openerp/sql_db.py
+++ openerp/sql_db.py
@@ -419,6 +419,9 @@ class ConnectionPool(object):
                     if not cnx.closed:
                         cnx.close()
                     continue
+                except psycopg2.DatabaseError:
+                    _logger.warning('Could not borrow connection (DatabaseError) at index %d: %r', i, cnx.dsn)
+                    continue
                 self._connections.pop(i)
                 self._connections.append((cnx, True))
                 self._debug('Existing connection found at index %d', i)
